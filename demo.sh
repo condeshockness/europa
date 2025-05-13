159d5c9a83c0523bc328c47b819c56b90def548d
Alfonsin <ramonsilas.dev@gmail.com>
2025-05-13 00:26:47 -0400

    ok

--- demo.sh
+++ demo.sh
@@ -1,9 +1,3 @@
 echo Hello from Bash!
 echo It is demo script.
-echo List of files in current directory:
-ls -lart
-
-echo List of processes on the server:
-ps aux
-
-echo Bye!
+apt install -y make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev
